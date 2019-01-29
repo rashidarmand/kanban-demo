a44888e976f3d9b967644c7b22bd93abd23b4639
Rashid H Armand <rashidharmand@gmail.com>
2019-01-29 15:26:13 -0500

    added notes for later

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
-export  default class App extends Component {
+const demoCards = ['Demo Card 1', 'Demo Card 2'];
+
+export default class App extends Component {
+  state = {
+    boards: [
+      {
+        name: 'Winnie',
+        color: '#8E6E95',
+        cards: demoCards
+      },
+      {
+        name: 'Bob',
+        color: '#35A95C',
+        cards: demoCards
+      },
+      {
+        name: 'Thomas',
+        color: '#344759',
+        cards: demoCards
+      },
+      {
+        name: 'George',
+        color: '#E8741E',
+        cards: demoCards
+      }
+    ]
+  }
   render() {
+    const { boards } = this.state;
+
     return (
       <div>
         <h1>Kanban</h1>
+        {boards.map(board => board.name)}
       </div>
     )
   }
